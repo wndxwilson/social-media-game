ee8cd23b2bbb706993ead1f86bdb1b8f40c57bea
Darryl Tan Kah Heng <41720545+darryltkh@users.noreply.github.com>
2020-05-09 20:59:47 +0800

    cabehiygcfoyegfiyshabcikahciurfuivnhaisncnasoicA
    C;oubciuhQW;OEDH;2OQE9EHD98WEYFI2UBRDIOU93RYRO8E4FWYVBH LUCHAISUBC IJNWDPIQAWJFIIE3

---
+++ PointsUpdater.py
@@ -0,0 +1,39 @@
+from re import search
+from random import randint
+
+class PointsUpdater:
+    def __init__(self, participant):
+        self.participant = participant
+        self.points = randint(0, 101)
+        self.rewardTier = {(0, 50): "Nothing", (51, 75): "A bear", (76, 100): "A trip to Sentosa"}
+
+    def updatePoints(self, hashtag, ig_caption):
+        hashtag = r"{}".format(hashtag)
+        if search(hashtag, ig_caption):
+            self.points = self.points + 1
+        print(self.points)
+    
+    def claimReward(self):
+        temp_dict = list(self.rewardTier.keys())
+        for x in range(len(temp_dict)):
+            minimum, maximum = temp_dict[x]
+            if minimum <= self.points <= maximum:
+                print(self.rewardTier[temp_dict[x]])
+
+    def addPoints(self, points):
+        self.points = self.points + points
+        print(self.points) 
+
+    def removePoints(self, points):
+        self.points = self.points - points
+        print(self.points)
+
+if __name__ == "__main__":
+    pu = PointsUpdater("Jenny")
+    print(pu.participant)
+    print(pu.points)
+    pu.addPoints(1)
+    pu.removePoints(1)
+    pu.updatePoints("#code4Corona", "Yay, I have just joined #code4Corona!!!")
+    pu.claimReward()
+
